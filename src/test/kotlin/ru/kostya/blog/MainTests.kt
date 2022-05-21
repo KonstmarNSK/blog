49814c8d0243d8813c2bed987045df563eb342ba
konstantin <konstmar@yandex.ru>
2022-05-21 15:02:08 +0700

    added spring security config

--- src/test/kotlin/ru/kostya/blog/MainTests.kt
+++ src/test/kotlin/ru/kostya/blog/MainTests.kt
@@ -1,10 +1,10 @@
-package com.example.demo
+package ru.kostya.blog
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class MainTests {
 
 	@Test
 	fun contextLoads() {
